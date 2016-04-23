d09ebc6fcef436668e6d080ecac22c055a005171
Denis BEURIVE <dbeurive.github@gmail.com>
2016-04-23 14:00:26 +0200

    Make test possible when the package is installed

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,15 @@
 <?php
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+// If we are developing the library, then the autoload script should be here: ../vendor/autoload.php.
+// Otherwise, it should be here: ../../../../autoload.php.
+
+$pathTokens = ['..', 'vendor', 'autoload.php'];
+$path = __DIR__ . DIRECTORY_SEPARATOR . join(DIRECTORY_SEPARATOR, $pathTokens);
+
+if (! file_exists($path)) {
+    array_unshift($pathTokens, '..', '..', '..');
+    $path = __DIR__ . DIRECTORY_SEPARATOR . join(DIRECTORY_SEPARATOR, $pathTokens);
+}
+
+require_once $path;
 
